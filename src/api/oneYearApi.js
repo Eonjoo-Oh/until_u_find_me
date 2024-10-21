c53f689c2166b79f8bb16573fecdff2dcf4b01b3
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-21 22:01:52 +0900

    api파일 분리

---
+++ src/api/oneYearApi.js
@@ -0,0 +1,31 @@
+export const fetchOneYearDogs = async () => {
+	const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
+
+	//일년전 오늘 날짜 구하기
+	const oneYearAgo = new Date();
+	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+	const oneYearAgoDate = oneYearAgo.toISOString().split('T')[0].replace(/-/g, '');
+
+	const baseUrl = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
+	
+	let page = 1;
+	let oneYearDogs = [];
+	let response = await fetch(`${baseUrl}&pageNo=${page}`);
+	let json = await response.json();
+	const totalCount = json.response.body.totalCount;
+	
+	oneYearDogs.push(...json.response.body.items.item);
+	
+	//항목이 100개 이상이면 pageno를 넘겨서 추가로 api 요청
+	if (totalCount > 100) {
+		page = 2;
+		while(page <= Math.ceil(totalCount / 100)) {
+			response = await fetch(`${baseUrl}&pageNo=${page}`);
+			json = await response.json();
+			oneYearDogs.push(...json.response.body.items.item);
+			page++;
+		}
+	} 
+	
+	return oneYearDogs;
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,41 +1,18 @@
 import { useState, useEffect } from "react";
+import { fetchOneYearDogs } from "../api/oneYearApi";
 
 function Home() {
-	const [oneYearAnimals, setOneYearAnimals] = useState([]);
-	const oneYearAgo = new Date();
-	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
-	const oneYearAgoDate = oneYearAgo.toISOString().split('T')[0].replace(/-/g, '');
-	let page = 1;
-
-	const getOneYearAnimals = async() => {
-		const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
-		const baseUrl = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
-		let response = await fetch(`${baseUrl}&pageNo=${page}`);
-		let json = await response.json();
-		const totalCount = json.response.body.totalCount;
-		let tempAnimal = [];
-		tempAnimal.push(...json.response.body.items.item);
-		
-		if (totalCount > 100) {
-			console.log("here");
-			page = 2;
-			while(page <= Math.ceil(totalCount / 100)) {
-				response = await fetch(`${baseUrl}&pageNo=${page}`);
-				json = await response.json();
-				tempAnimal.push(...json.response.body.items.item);
-				page++;
-			}
-		} //항목이 100개 이상이면 pageno를 넘겨서 추가로 api 요청
-	
-		setOneYearAnimals(tempAnimal);
-	};
+	const [oneYearDogs, setOneYearDogs] = useState([]);
 
 	useEffect(() => {
-		getOneYearAnimals();
+		const fetchData = async () => {
+		  const oneYearData = await fetchOneYearDogs();
+		  setOneYearDogs(oneYearData);
+		}
+		fetchData();
 	  }, []);
 
-	console.log(oneYearAnimals);
-	
+	console.log(oneYearDogs);
 }
 
 export default Home;
