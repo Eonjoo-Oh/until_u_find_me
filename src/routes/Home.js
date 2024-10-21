4eee579eebe4b94b8441350bf078476513f1a2a4
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-21 21:04:41 +0900

    변수이름 변경

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
 function Home() {
-	const [animals, setAnimals] = useState([]);
+	const [oneYearAnimals, setOneYearAnimals] = useState([]);
 	const oneYearAgo = new Date();
 	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
 	const oneYearAgoDate = oneYearAgo.toISOString().split('T')[0].replace(/-/g, '');
 	let page = 1;
 
-	const getAnimals = async() => {
+	const getOneYearAnimals = async() => {
 		const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
 		const baseUrl = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
 		let response = await fetch(`${baseUrl}&pageNo=${page}`);
@@ -28,14 +28,14 @@ function Home() {
 			}
 		} //항목이 100개 이상이면 pageno를 넘겨서 추가로 api 요청
 	
-		setAnimals(tempAnimal);
+		setOneYearAnimals(tempAnimal);
 	};
 
 	useEffect(() => {
-		getAnimals();
+		getOneYearAnimals();
 	  }, []);
 
-	console.log(animals);
+	console.log(oneYearAnimals);
 	
 }
 
