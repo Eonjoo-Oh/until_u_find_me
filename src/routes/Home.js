2514b60ab5dc70d66badb004f642fc2cc6a338fb
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-21 20:58:47 +0900

    한번에 요청할 수 있는 갯수 초과시 다음 페이지 불러오는 로직 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,19 +5,38 @@ function Home() {
 	const oneYearAgo = new Date();
 	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
 	const oneYearAgoDate = oneYearAgo.toISOString().split('T')[0].replace(/-/g, '');
+	let page = 1;
 
 	const getAnimals = async() => {
 		const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
-		const url = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&pageNo=1&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
+		const baseUrl = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
 		//토탈카운트가 100개 이상이면 pageno를 넘겨서 fetch하는 로직 필요
-		const response = await fetch(url);
-		const json = await response.json();
-		console.log(json);
-		// console.log(json.AbdmAnimalProtect[1].row);
-		// setAnimals(json.AbdmAnimalProtect[1].row);
+		let response = await fetch(`${baseUrl}&pageNo=${page}`);
+		let json = await response.json();
+		const totalCount = json.response.body.totalCount;
+		console.log(totalCount);
+		let tempAnimal = [];
+		tempAnimal.push(...json.response.body.items.item);
+		
+		if (totalCount > 100) {
+			console.log("here");
+			page = 2;
+			while(page <= Math.ceil(totalCount / 100)) {
+				response = await fetch(`${baseUrl}&pageNo=${page}`);
+				json = await response.json();
+				tempAnimal.push(...json.response.body.items.item);
+				page++;
+			}
+		}
+	
+		setAnimals(tempAnimal);
 	};
 
-	useEffect(() => {getAnimals()}, []);
+	useEffect(() => {
+		getAnimals();
+	  }, []);
+
+	console.log(animals);
 	
 }
 
