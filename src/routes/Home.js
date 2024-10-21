55a4379484545a5a8eabde39b089c17c3d94d708
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-21 20:13:16 +0900

    api 변경, 1년전 오늘날짜로  필터링해서 요청

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,14 +2,19 @@ import { useState, useEffect } from "react";
 
 function Home() {
 	const [animals, setAnimals] = useState([]);
+	const oneYearAgo = new Date();
+	oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+	const oneYearAgoDate = oneYearAgo.toISOString().split('T')[0].replace(/-/g, '');
 
 	const getAnimals = async() => {
 		const apiKey = process.env.REACT_APP_ANIMAL_API_KEY;
-		const url = `https://openapi.gg.go.kr/AbdmAnimalProtect?KEY=${apiKey}&Type=json`;
-
+		const url = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=${oneYearAgoDate}&endde=${oneYearAgoDate}&upkind=417000&pageNo=1&numOfRows=100&state=protect&kind-417000&serviceKey=${apiKey}&_type=json`;
+		//토탈카운트가 100개 이상이면 pageno를 넘겨서 fetch하는 로직 필요
 		const response = await fetch(url);
 		const json = await response.json();
 		console.log(json);
+		// console.log(json.AbdmAnimalProtect[1].row);
+		// setAnimals(json.AbdmAnimalProtect[1].row);
 	};
 
 	useEffect(() => {getAnimals()}, []);
