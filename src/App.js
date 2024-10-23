a1256a510c3459dec0f41c24067fa1ff01370e5f
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-23 22:48:30 +0900

    reset.css 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Header from "./components/Header";
+import './styles/reset.css';
 
 function App() {
   return (
     <Router>
       < Header/>
-
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
