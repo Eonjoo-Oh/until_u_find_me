81aeb7ae0b0a9c505d46c25638779096fbc36e68
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-23 23:27:20 +0900

    header에 css, 반응형 css 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 				<h1>어서와요 기다렸어요</h1>
 				<h3 className='sub-title'>until you find me</h3>
 			</div>
-			<nav>
+			<nav className='navigation'>
 				<div className='nav-buttons'>
 					<button className='nav-button'>1주년</button>
 					<button className='nav-button'>가장 오래 기다린</button>
