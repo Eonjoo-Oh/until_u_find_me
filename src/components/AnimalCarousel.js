92c7637ffecd59f62dba4eda666c97c726e151bb
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-26 19:21:55 +0900

    한칸씩 넘어가는 무한 캐러셀 제작

--- src/components/AnimalCarousel.js
+++ src/components/AnimalCarousel.js
@@ -3,20 +3,30 @@ import AnimalCard from './AnimalCard';
 import '../styles/animalCarousel.css';
 
 function AnmialCarousel({ animals }) {
-	const [currentIndex, setCurrentIndex] = useState(0);
 	const itemToShow = 4;
+	const [currentIndex, setCurrentIndex] = useState(itemToShow);
+	
+	// animals가 없거나, itemToShow보다 작은 경우 처리방법 추가해야됨
+	// if (animals.length === 0) {
+	// 	return (
+	// 		<div className='blank-animal'>
+	// 			{"좋은날이에요! 일년 전 오늘 입소한 친구들이 없어요"}
+	// 		</div>
+	// 	);
+	// }
+	// (animals.length < 4 ? itemToShow = animals.length : itemToShow = 4)
+
+	const infiniteAnimals = [...animals.slice(-itemToShow), ...animals, ...animals.slice(0, itemToShow)];
+	console.log("infiniteAnimals : ", infiniteAnimals)
 
 	const prevSlide = () => {
 		setCurrentIndex((prevIdx) => 
-			// prevIdx < 0 ? animals.length - 1 : prevIdx - 1
-			prevIdx - itemToShow < 0 ? animals.length - itemToShow : prevIdx - itemToShow
+			prevIdx === 0 ? Math.floor(animals.length - 1) : prevIdx - 1
 		);
-	};
-
+	}; 
 	const nextSlide = () => {
 		setCurrentIndex((nextIdx) => 
-			// nextIdx 
-			nextIdx + itemToShow >= animals.length ? 0 : nextIdx + itemToShow
+			infiniteAnimals.length - currentIndex === itemToShow ? itemToShow : currentIndex + 1
 		);
 	};
 	console.log("currentIdx: ", currentIndex);
@@ -24,7 +34,8 @@ function AnmialCarousel({ animals }) {
 		<div className='carousel'>
 			<button onClick={prevSlide} className='prev-btn'>{"<"}</button>
 			<div className='carousel-track'>
-				{animals.slice(currentIndex, currentIndex + itemToShow).map((animal, index) => (
+				{/* 배열하나 만들어서 그 배열에 채워넣기 */}
+				{infiniteAnimals.slice(currentIndex, currentIndex + itemToShow).map((animal, index) => (
 					<AnimalCard key={index} animal={animal} />
 				))}
 			</div>
