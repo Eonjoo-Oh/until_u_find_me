be113cef3f82f96c43b2e1b534b23397bcb1f6be
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-10-28 17:49:23 +0900

    animalDetail page 상태 전달 테스트

--- src/routes/AnimalDetail.js
+++ src/routes/AnimalDetail.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 function AnimalDetail() {
-
+	const location = useLocation();
+	const { animal } = location.state || {};
+	console.log(animal);
 }
 
 export default AnimalDetail;
